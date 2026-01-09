25738713621c414337d013450bac95ef9de76f21
robot999 <1244250292@qq.com>
2026-01-09 19:56:59 +0800

    initial commit

---
+++ src/main/java/com/jiajin/core/SimpleRestServerApplication.java
@@ -0,0 +1,13 @@
+package com.jiajin.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRestServerApplication.class, args);
+    }
+
+}
